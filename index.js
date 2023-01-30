b1040a473ac05638d9e50e37dbbbb0aefa0ed0ab
Nadia K <81541193+nkrasno@users.noreply.github.com>
2023-01-30 12:39:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ userName.oninvalid = function (e) {
 
 //Chequear que los campos estén completos y validarlos al salir del input
 function checkEmptyField(arg) {
-    if (inputs[arg].value == 0) {
+    if (inputs[arg].value === 0) {
         addErrorStyles(arg);
     } else {
         checkValidity(arg)
@@ -59,11 +59,12 @@ checkPassword.onfocus = function () { resetFieldStyles(3) };
 
 //Chequear validez según el campo y agregar estilos 
 function checkValidity(arg) {
+    let userNameRecortado = userName.value.trim();
+    const validName = /^[a-zA-ZñÑáéíóúÁÉÍÓÚçÇ\s]+$/;
+    const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     switch (arg) {
         case 0:
-            let user_name_recortado = userName.value.trim()
-            let validName = /^[a-zA-ZñÑáéíóúÁÉÍÓÚçÇ\s]+$/;
-            if (!validName.test(user_name_recortado)) {
+            if (!validName.test(userNameRecortado)) {
                 addErrorStyles(arg);
                 smallError[arg].innerHTML = "El nombre no puede contener números";
             } else {
@@ -71,7 +72,6 @@ function checkValidity(arg) {
             }
             break;
         case 1:
-            let validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
             if (!validEmail.test(email.value)) {
                 addErrorStyles(arg);
                 smallError[arg].innerHTML = "Email inválido";
@@ -88,7 +88,7 @@ function checkValidity(arg) {
             }
             break;
         case 3:
-            if (password.value != checkPassword.value) {
+            if (password.value !== checkPassword.value) {
                 addErrorStyles(arg);
                 smallError[arg].innerHTML = "Las contraseñas no coinciden";
             } else {
@@ -100,8 +100,123 @@ function checkValidity(arg) {
 
 //Revisa si los inputs son todos válidos
 function checkAllFields() {
-    let validInputs = document.querySelectorAll('.valid');
-    if (validInputs.length != 4) {
+    const validInputs = document.querySelectorAll('.valid');
+    if (validInputs.length !== 4) {
+        checkEmptyField(0);
+        checkEmptyField(1);
+        checkEmptyField(2);
+        checkEmptyField(3);
+        return false;
+    } else {
+        alert("La inscripción ha sido correcta");
+        return true;
+    }
+}
+
+button.addEventListener("click", checkAllFields);const button = document.getElementById('button');
+const userName = document.getElementById('name');
+const email = document.getElementById('mail');
+const password = document.getElementById('password');
+const checkPassword = document.getElementById('check-password');
+const inputs = document.querySelectorAll('input');
+const smallError = document.querySelectorAll('p');
+const img = document.querySelectorAll('img');
+
+//Agregar estilos si hay un error
+function addErrorStyles(arg) {
+    smallError[arg].style.visibility = 'visible';
+    smallError[arg].innerHTML = "Rellene este campo";
+    inputs[arg].classList.add('not-valid');
+    img[arg].src = "images/error-icon.svg";
+    img[arg].style.visibility = 'visible';
+    return false;
+}
+
+//Agregar estilos si NO hay un error
+function addValidStyles(arg) {
+    inputs[arg].classList.add('valid');
+    img[arg].src = "images/success-icon.svg";
+    img[arg].style.visibility = 'visible';
+    return true;
+}
+
+//Personalizar tooltip de nombre si no cumple el patrón
+userName.oninvalid = function (e) {
+    e.target.setCustomValidity("El nombre no puede contener números");
+}
+
+//Chequear que los campos estén completos y validarlos al salir del input
+function checkEmptyField(arg) {
+    if (inputs[arg].value === 0) {
+        addErrorStyles(arg);
+    } else {
+        checkValidity(arg)
+    }
+}
+
+userName.onblur = function () { checkEmptyField(0) };
+email.onblur = function () { checkEmptyField(1) };
+password.onblur = function () { checkEmptyField(2) };
+checkPassword.onblur = function () { checkEmptyField(3) };
+
+//Resetear estilos al enfocar un input
+function resetFieldStyles(arg) {
+    smallError[arg].style.visibility = 'hidden';
+    inputs[arg].classList.remove('not-valid');
+    inputs[arg].classList.remove('valid');
+    img[arg].style.visibility = 'hidden';
+}
+
+userName.onfocus = function () { resetFieldStyles(0) };
+email.onfocus = function () { resetFieldStyles(1) };
+password.onfocus = function () { resetFieldStyles(2) };
+checkPassword.onfocus = function () { resetFieldStyles(3) };
+
+//Chequear validez según el campo y agregar estilos 
+function checkValidity(arg) {
+    let userNameRecortado = userName.value.trim();
+    const validName = /^[a-zA-ZñÑáéíóúÁÉÍÓÚçÇ\s]+$/;
+    const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    switch (arg) {
+        case 0:
+            if (!validName.test(userNameRecortado)) {
+                addErrorStyles(arg);
+                smallError[arg].innerHTML = "El nombre no puede contener números";
+            } else {
+                addValidStyles(arg);
+            }
+            break;
+        case 1:
+            if (!validEmail.test(email.value)) {
+                addErrorStyles(arg);
+                smallError[arg].innerHTML = "Email inválido";
+            } else {
+                addValidStyles(arg);
+            }
+            break;
+        case 2:
+            if (password.value.length > 8) {
+                addErrorStyles(arg);
+                smallError[arg].innerHTML = "No debe tener más de 8 caracteres";
+            } else {
+                addValidStyles(arg);
+            }
+            break;
+        case 3:
+            if (password.value !== checkPassword.value) {
+                addErrorStyles(arg);
+                smallError[arg].innerHTML = "Las contraseñas no coinciden";
+            } else {
+                addValidStyles(arg);
+            }
+            break;
+    }
+}
+
+//Revisa si los inputs son todos válidos
+function checkAllFields() {
+    const validInputs = document.querySelectorAll('.valid');
+    if (validInputs.length !== 4) {
         checkEmptyField(0);
         checkEmptyField(1);
         checkEmptyField(2);
